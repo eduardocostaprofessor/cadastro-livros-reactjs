cde0da9a9e2b9bfa1c494b029d2e075b69cbb6ec
Eduardo Costa <eduardocostaprofessor@gmail.com>
2019-10-19 00:40:13 -0300

    Insere url da api

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,8 @@
-export const API_URL = 'http://localhost:3002/api';
+const ENV = (window.location.href.indexOf('localhost') === -1)? 'prod' : 'dev';
+
+export const API_URL = (ENV === 'dev') 
+? 'http://localhost:3002/api' 
+: 'https://cadastro-livros-senai-backend.herokuapp.com/api';
+
+
+console.log(API_URL);
